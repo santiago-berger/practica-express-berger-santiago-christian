8ad3a821ef456f726b1d9bdc5f2ceb15e6c2d50e
santiago-berger <santiago.christian.berger@gmail.com>
2026-06-14 17:48:48 -0300

    feat: se implementó el endpoint GET para obtener todos los personajes

--- src/controllers/personajes.controllers.js
+++ src/controllers/personajes.controllers.js
@@ -1,6 +1,8 @@
 import { personajes } from "../data/personajes.js";
 
-export const obtenerTodosLosPersonajes = (req, res) => {};
+export const obtenerTodosLosPersonajes = (req, res) => {
+    return res.status(200).json(personajes);
+};
 export const obtenerUnPersonajePorId = (req, res) => {};
 export const crearUnPersonaje = (req, res) => {};
 export const editarUnPersonaje = (req, res) => {};
