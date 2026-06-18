e1fa71d6441716484c348e97310affcdbac7c7f8
santiago-berger <santiago.christian.berger@gmail.com>
2026-06-18 09:47:01 -0300

    feat: se implementó el endpoint GET para obtener un personaje por id

--- src/controllers/personajes.controllers.js
+++ src/controllers/personajes.controllers.js
@@ -3,7 +3,25 @@ import { personajes } from "../data/personajes.js";
 export const obtenerTodosLosPersonajes = (req, res) => {
     return res.status(200).json(personajes);
 };
-export const obtenerUnPersonajePorId = (req, res) => {};
+export const obtenerUnPersonajePorId = (req, res) => {
+    const id = Number(req.params.id);
+
+    if (Number.isNaN(id)) {
+        return res.status(400).json({
+            message: "El id debe ser un número válido"
+        })   
+    }
+
+    const personaje = personajes.find((p) => p.id === id);
+
+    if (!personaje) {
+        return res.status(404).json({
+            message: `No se encontró el personaje con el id #${id}`,
+        });
+    }
+
+    return res.status(200).json(personaje);
+};
 export const crearUnPersonaje = (req, res) => {};
 export const editarUnPersonaje = (req, res) => {};
 export const eliminarUnPersonaje = (req, res) => {};
